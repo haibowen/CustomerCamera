81b471be6bbf65abb044e1c7cb3009cc2ec969ce
wenhaibo <1461154748@qq.com>
2020-08-03 15:07:08 +0800

    自定义相机

---
+++ app/src/main/java/cn/com/agree/mycustomerdemo/CustomCameraActivity.java
@@ -0,0 +1,97 @@
+package cn.com.agree.mycustomerdemo;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.WindowManager;
+
+
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import cn.com.agree.customer_camera.CameraView;
+
+/**
+ * Created by Allen on 2017/11/20.
+ * 相机界面Activity
+ */
+
+public class CustomCameraActivity extends AppCompatActivity {
+    private CameraView cameraView;
+    private String mPath;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //去掉状态栏
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        cameraView = new CameraView(this);
+        setContentView(cameraView);
+        //去掉ActionBar
+        getSupportActionBar().hide();
+        mPath = getIntent().getStringExtra("path");
+        cameraView.setCameraListener(cameraListener);
+    }
+
+    private CameraView.CameraListener cameraListener = new CameraView.CameraListener() {
+        @Override
+        public void onCapture(Bitmap bitmap) {
+            File file = new File(mPath);
+            if (!file.getParentFile().exists()) {
+                file.getParentFile().mkdirs();
+            }
+
+            try {
+                FileOutputStream out = new FileOutputStream(file);
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+                out.flush();
+                out.close();
+            } catch (IOException e) {
+            }
+
+            if (file.exists()) {
+                Intent data = new Intent();
+                data.setData(Uri.parse(mPath));
+                setResult(RESULT_OK, data);
+            }
+
+            finish();
+        }
+
+        @Override
+        public void onCameraClose() {
+            finish();
+        }
+
+        @Override
+        public void onCameraError(Throwable th) {
+            onCameraClose();
+        }
+    };
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        cameraView.onResume();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        cameraView.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+    }
+}
---
+++ app/src/main/java/cn/com/agree/mycustomerdemo/MainActivity.java
@@ -0,0 +1,186 @@
+package cn.com.agree.mycustomerdemo;
+
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.img_photo)
+    ImageView imgPhoto;
+    @BindView(R.id.system_camera)
+    Button systemCamera;
+    @BindView(R.id.custom_camera)
+    Button customCamera;
+    private static final int REQUEST_CODE_TAKE_PICTURE_NO_URI = 0;
+    private static final int REQUEST_CODE_TAKE_PICTURE_URI = 1;
+    private static final int REQUEST_CODE_TAKE_PICTURE_CUSTOM_CAMERA = 2;
+    private Uri photoUri;
+    private Bitmap bitmap;
+
+    List<String> permissionList = new ArrayList<>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        getSupportActionBar().hide();
+
+
+        // 判断权限是否已经授予，没有就把该权限添加到列表中
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.CAMERA);
+        }
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+        }
+        // 如果列表为空，就是全部权限都获取了，不用再次获取了。不为空就去申请权限
+        if (!permissionList.isEmpty()) {
+            ActivityCompat.requestPermissions(this,
+                    permissionList.toArray(new String[permissionList.size()]), 1);
+        } else {
+            Toast.makeText(this, "多个权限你都有了，不用再次申请", Toast.LENGTH_LONG).show();
+        }
+
+
+    }
+
+
+    @OnClick({R.id.img_photo, R.id.system_camera, R.id.custom_camera})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.img_photo://照片
+                break;
+            case R.id.system_camera://系统相机
+                showWaySelectDialog();
+                break;
+            case R.id.custom_camera://自定义相机
+                Intent intent = new Intent(MainActivity.this, CustomCameraActivity.class);
+                String path = getExternalFilesDir(Environment.DIRECTORY_PICTURES) + "/camera_" + System.currentTimeMillis() + ".jpg";
+                intent.putExtra("path", path);
+                startActivityForResult(intent, REQUEST_CODE_TAKE_PICTURE_CUSTOM_CAMERA);
+                break;
+            default:
+                break;
+
+        }
+    }
+
+
+    /**
+     * 显示选择系统相机方式的对话框
+     */
+    private void showWaySelectDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("选择方式");
+        builder.setMessage("选择调用系统相机的方式，方式一不指定Uri,方式二指定Uri");
+        builder.setNegativeButton("方式一", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent openCameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE); //系统常量， 启动相机的关键
+                startActivityForResult(openCameraIntent, REQUEST_CODE_TAKE_PICTURE_NO_URI); // 参数常量为自定义的request code, 在取返回结果时有用
+            }
+        });
+        builder.setPositiveButton("方式二", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent openCameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                photoUri = Uri.fromFile(getSavePhotoFile());
+                openCameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                startActivityForResult(openCameraIntent, REQUEST_CODE_TAKE_PICTURE_URI);
+            }
+        });
+        AlertDialog dialog = builder.show();
+        dialog.setCanceledOnTouchOutside(false);
+        dialog.show();
+    }
+
+
+    /**
+     * 获取保存拍照后图片保存的File
+     *
+     * @return
+     */
+    private File getSavePhotoFile() {
+        String photoPath = getExternalFilesDir(Environment.DIRECTORY_PICTURES) + "/camera_" + System.currentTimeMillis() + ".jpg";
+        File file = new File(photoPath);
+        if (!file.getParentFile().exists()) {
+            file.getParentFile().mkdirs();
+        }
+        return file;
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode != RESULT_OK) {//判断结果是否成功
+            return;
+        }
+
+        ContentResolver resolver = getContentResolver();
+        switch (requestCode) {
+            case REQUEST_CODE_TAKE_PICTURE_NO_URI://不指定Uri方式的系统相机回调
+                bitmap = data.getParcelableExtra("data");
+                imgPhoto.setImageBitmap(bitmap);
+                break;
+            case REQUEST_CODE_TAKE_PICTURE_URI://指定Uri方式的系统相机回调
+
+                try {
+                    bitmap = BitmapFactory.decodeStream(resolver.openInputStream(photoUri));
+                    imgPhoto.setImageBitmap(bitmap);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                break;
+            case REQUEST_CODE_TAKE_PICTURE_CUSTOM_CAMERA:
+                String path = data.getDataString();
+                Bitmap bitmap = BitmapFactory.decodeFile(path);
+                imgPhoto.setImageBitmap(bitmap);
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+    }
+}
+
